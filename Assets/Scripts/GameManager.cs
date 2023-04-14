eef677c13965de404d44263a5a06d280796c88d7
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-14 23:56:33 +0300

    TMPro library added; TextMeshProUGUI variable declared in GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private float spawnRate = 1.0f;
+    private int score;
 
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
