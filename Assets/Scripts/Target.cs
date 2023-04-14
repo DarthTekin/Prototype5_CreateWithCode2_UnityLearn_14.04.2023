6359834ad822fb1dfe725a20db14e9d36094918c
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-14 16:58:42 +0300

    Target.cs cleaned up by new varibles and methods added in.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+
     private Rigidbody targetRb;
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
 
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddForce(Vector3.up * Random.Range(minSpeed, maxSpeed), ForceMode.Impulse);
         targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
 
         transform.position = new Vector3(Random.Range(-4, 4), -6);
