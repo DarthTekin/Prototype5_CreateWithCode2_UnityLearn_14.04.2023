bc49432f143638d91267061074d61925dff54cb5
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-15 11:48:59 +0300

    TextMeshProUGUI variable added in Hierarchy, re-named Game Over and disabled; Boolean gameobject.SetActive() method added in GameManager.cs.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
 
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,8 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
