5671778a1a5bcc7564b590257be82b08b973ff82
tekinteks@gmail.com <tekinteks@gmail.com>
2023-04-20 17:43:24 +0300

    Input.GetMouseButtonDown(0) method added in ClickAndSwipe.cs to call UpdateComponents() method and UpdateMousePosition() Method.

--- Assets/Scripts/ClickAndSwipe.cs
+++ Assets/Scripts/ClickAndSwipe.cs
@@ -29,12 +29,42 @@ public class ClickAndSwipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameManager.isGameActive)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                swiping = true;
+                UpdateComponents();
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                swiping = false;
+                UpdateComponents();
+            }
+            if (swiping)
+            {
+                UpdateMousePosition();
+            }
+        }
     }
 
     void UpdateMousePosition()
     {
-        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10.0f));
+        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
         transform.position = mousePos;
+    }
+
+    void UpdateComponents()
+    {
+        trail.enabled = swiping;
+        col.enabled = swiping;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Target>())
+        {
+
+        }
     }
 }
